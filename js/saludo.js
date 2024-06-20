26b9d4cc68b7a5659dbebd434d46ccc519d6faeb
CatalinaMarcos <cmarcosaslanian@udesa.edu.ar>
2024-06-19 22:43:55 -0300

    termine la parte de saludo de login

---
+++ js/saludo.js
@@ -0,0 +1,15 @@
+let emailUsuario = localStorage.getItem("userEmail")
+
+let login = document.querySelector(".login")
+let registro = document.querySelector(".registro")
+
+let mensaje = "Bienvenido " + emailUsuario;
+
+if (emailUsuario){
+    login.innerText = mensaje;
+    registro.innerHTML = `<a href="">Logout</a>`
+} else {
+    login.style.display = "inline"
+    registro.style.display = "inline"
+}
+
